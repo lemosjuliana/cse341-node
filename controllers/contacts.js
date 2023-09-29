6a3feb9b4b87411e3c49e77dc10e17f691ce30e6
lemosjuliana <lemos.julianajesus@gmail.com>
2023-09-29 10:04:15 -0300

    L03 - POST, PUT, and DELETE

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,17 +5,6 @@ const ObjectId = require('mongodb').ObjectId; // is a type provided by the Mongo
 // Function that handles a GET request.
 // It first retrieves the collection named 
 // -contacts- from the database:
-// const getAll = async (req, res, next) => {
-//   try {
-//     const result = await mongodb.getDb().db().collection('contacts').find({}).toArray();
-//     res.setHeader('Content-Type', 'application/json');
-//     res.status(200).json(result);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
 const getAll = async (req, res, next) => {
   try {
     const result = await mongodb.getDb().db('cse-341').collection('contacts').find().toArray();
@@ -41,5 +30,49 @@ const getSingle = async (req, res, next) => {
       res.status(200).json(lists[0]);
     });
   };
+
+// Create a POST route for creating new contacts that returns the ID 
+// of the new contact and a 201 status
+const createContact = async (req, res) => {
+  const contact = { firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, favoriteColor: req.body.favoriteColor, birthday: req.body.birthday };
+  const response = await mongodb.getDb().db('cse-341').collection('contacts').insertOne(contact);
+  if (response.acknowledged){
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error);
+  };
   
-  module.exports = { getAll, getSingle };
+};
+
+// Create a PUT route for updating a contact that 
+// returns a 204 status
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = { firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, favoriteColor: req.body.favoriteColor, birthday: req.body.birthday };
+  const response = await mongodb.getDb().db('cse-341').collection('contacts').replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error);
+  };
+  
+};
+
+// Create a DELETE route for deleting a contact that 
+// returns a 200 status
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db('cse-341').collection('contacts').deleteOne({ _id: userId });
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(200).send();
+  } else {
+    res.status(500).json(response.error);
+  };
+  
+};
+
+  
+  module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,6 @@
 // Lesson 02
 
+
 const routes = require('express').Router();
 const contactsController = require('../controllers/contacts');
  
@@ -11,5 +12,17 @@ routes.get('/:id', contactsController.getSingle);
 
 // To view everything, you may also use an extension like 
 // Thunder Client, instead of just seeing it on the browser :)
+
+// Create a POST route for creating new contacts that 
+// returns the ID of the new contact and a 201 status:
+routes.post('/', contactsController.createContact);
+
+// Create a PUT route for updating a contact that 
+// returns a 204 status
+routes.put('/:id', contactsController.updateContact);
+
+// Create a DELETE route for deleting a contact that
+// returns a 200 status
+routes.delete('/:id', contactsController.deleteContact);
 
 module.exports = routes;
