302790e934171ba871a6ac642e79aa5ed9fc3dcc
lemosjuliana <lemos.julianajesus@gmail.com>
2023-10-06 18:22:12 -0300

    L04 - Contacts Frontend

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -19,60 +19,87 @@ const getAll = async (req, res, next) => {
   
 // Similar to -getAll- function.
 const getSingle = async (req, res, next) => {
+  try {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb
       .getDb()
       .db('cse-341')
       .collection('contacts')
-      .find({ _id: userId });
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
-    });
-  };
+      .find({ _id: userId })
+      .toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 // Create a POST route for creating new contacts that returns the ID 
 // of the new contact and a 201 status
 const createContact = async (req, res) => {
-  const contact = { firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, favoriteColor: req.body.favoriteColor, birthday: req.body.birthday };
-  const response = await mongodb.getDb().db('cse-341').collection('contacts').insertOne(contact);
-  if (response.acknowledged){
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error);
-  };
-  
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db('cse-341').collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 // Create a PUT route for updating a contact that 
 // returns a 204 status
 const updateContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const contact = { firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, favoriteColor: req.body.favoriteColor, birthday: req.body.birthday };
-  const response = await mongodb.getDb().db('cse-341').collection('contacts').replaceOne({ _id: userId }, contact);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error);
-  };
-  
+  try {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db('cse-341').collection('contacts').replaceOne({ _id: userId }, contact);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 // Create a DELETE route for deleting a contact that 
 // returns a 200 status
 
 const deleteContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('cse-341').collection('contacts').deleteOne({ _id: userId });
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(200).send();
-  } else {
-    res.status(500).json(response.error);
-  };
-  
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db('cse-341').collection('contacts').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(500).json(response.error);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
+
 
   
   module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
